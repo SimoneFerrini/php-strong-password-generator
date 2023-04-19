e13b9f219c175305b08c815dbaa952ec424b9e87
SimoneFerrini <ferrini.simone4@gmail.com>
2023-04-19 16:03:05 +0200

    ended milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+    $pswLenght = $_GET['userPswLenght'] ?? 0 ;
+
+    function randomPSW($lenght){
+        $password = [];
+        $allChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz@#$&*';
+        for($i=0; $i < $lenght; $i++ ){
+            $char = $allChar[rand(0,66)];
+            $password[] = $char;
+            }
+        return implode($password);
+    };
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -14,13 +28,15 @@
     <div class="container">
         <h1>Generatore di password:</h1>
         <form action="index.php" method="GET">
-            <input type="number" placeholder="Insert password lenght">
+            <input type="number" placeholder="Insert password lenght" name="userPswLenght">
             <input type="submit" >
         </form>    
         <br>
         <hr>
         <br>
-        
+
+        <?php echo randomPSW($pswLenght)?>
+
     </div>
 </body>
 </html>
