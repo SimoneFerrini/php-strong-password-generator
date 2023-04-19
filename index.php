600484c26835c40f079a0a0fee98ca61fc8899c1
SimoneFerrini <ferrini.simone4@gmail.com>
2023-04-19 15:33:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+    
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1>Generatore di password:</h1>
+        <form action="index.php" method="GET">
+            <input type="number" placeholder="Insert password lenght">
+            <input type="submit" >
+        </form>    
+        <br>
+        <hr>
+        <br>
+        
+    </div>
+</body>
+</html>
